22a43855f24013047e6f16b16421c1fca7c0bd9e
Max forouzan <atabar@woolworths.com.au>
2021-12-17 15:09:59 +1100

    add sreamlit test

---
+++ stock_analysis_app.py
@@ -0,0 +1,16 @@
+# Test program
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+st.markdown("""---""")
+st.markdown("""Hi- This is test""")
+
+# write the header
+col1, col2, col3 = st.columns(3)
+col1.write("### Spend")
+col2.write("### Redemtion Rate")
+col3.write("### Average Cost")
--- tt.py
+++
@@ -1,7 +0,0 @@
-# Test program
-
-import numpy as np
-import matplotlib.pyplot as plt
-x=range(1,10)
-
-plt.plot(x,np.cos(x))
